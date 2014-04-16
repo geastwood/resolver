2d2c63b0c7d04a9da6bfc4bdc826496f8f764aa8
Fei Liu <liu.feiwood@gmail.com>
2014-04-16 10:39:50 +0200

    modify function signiture

--- src/resovler.js
+++ src/resovler.js
@@ -1,25 +1,42 @@
 var resolver = (function() {
 
+    'use strict';
 
     var MODULE_NAME_REGEX = /(\S+?)\.(\S+)/;
 
+    return function resolve(target, name) {
 
-    return function resolve(target, name, options) {
+        var action = 'get', obj, toSet = arguments.length === 3;
+        if (!name) {
+            throw new Error('module name must be specified.');
+        }
 
-        // TODO: check name
+        if (toSet) {
+            action = 'set';
+            obj = arguments[2];
+        }
 
         var parse = MODULE_NAME_REGEX.exec(name);
         var hasSubmodule = parse !== null;
 
         if (hasSubmodule) {
+
             target[parse[1]] = target[parse[1]] || {};
-            return resolve(target[parse[1]], parse[2], options);
+
+            if (toSet) {
+                return resolve(target[parse[1]], parse[2], obj);
+            } else {
+                return resolve(target[parse[1]], parse[2]);
+            }
         }
 
-        if (options.action === 'get') {
+        if (action === 'get') {
             return target[name];
-        } else if (options.action === 'set') {
-            target[name] = options.obj;
+        } else if (action === 'set') {
+            if (typeof obj === undefined) {
+                throw new Error('Set action with an empty object.');
+            }
+            target[name] = obj;
         } else {
             throw new Error('Failed to resolve.');
         }
--- tests/resolver.js
+++ tests/resolver.js
@@ -2,20 +2,29 @@ describe('resolver', function() {
     it('should be a function', function() {
         expect(typeof resolver).toBe('function');
     });
+    it('should pass a name to resolve', function() {
+        expect(resolver).toThrow();
+    });
 });
+
 describe('should resolve correctly', function() {
 
     var root, obj = {a: [], b: false};
 
     beforeEach(function() {
         root = {name: 'root element'};
-        resolver(root, 'foo.bar.foobar', {action: 'set', obj: obj});
+        resolver(root, 'foo.bar.foobar', obj);
+        resolver(root, 'f@#.ba(.foobar', obj);
     });
 
     it('should resolve correctly', function() {
-        var rst = resolver(root, 'foo.bar.foobar', {action: 'get'});
-        var rst1 = resolver(root, 'foo', {action: 'get'});
+        expect(root.name).toBe('root element');
+
+        var rst = resolver(root, 'foo.bar.foobar');
+        var rst1 = resolver(root, 'foo');
+        var rst2 = resolver(root, 'f@#');
         expect(rst).toEqual(obj);
         expect(rst1.bar.foobar).toEqual(obj);
+        expect(rst2['ba('].foobar).toEqual(obj);
     });
 });
